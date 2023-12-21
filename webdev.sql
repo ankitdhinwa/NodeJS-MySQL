a4d96a1e271e1714bf0cc8d18cc1ea58a87b584d
ankitdhinwa <akdhinwa91@gmail.com>
2023-12-21 13:21:58 +0530

    SQL file

---
+++ webdev.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 21, 2023 at 08:50 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webdev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webdev`
+--
+
+CREATE TABLE `webdev` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `webdev`
+--
+
+INSERT INTO `webdev` (`id`, `username`, `email`, `password`) VALUES
+(1, 'ankit', 'ankit@email.com', '$2a$08$EAvwpjP6IHpIp75kZr/qVuPqWRJMNIBD/Ub0oPneoHySx4pHi1j.i');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `webdev`
+--
+ALTER TABLE `webdev`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `webdev`
+--
+ALTER TABLE `webdev`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
